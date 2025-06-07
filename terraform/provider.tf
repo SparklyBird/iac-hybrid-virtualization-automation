3d64ff5ed4e14b966a83cd4730b39f1f42eb4002
SparklyBird <70488955+SparklyBird@users.noreply.github.com>
2025-06-08 01:01:24 +0300

    Create provider.tf

---
+++ terraform/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "Telmate/proxmox"
+      version = "2.9.14"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url      = "https://[PROXMOX_ZEROTIER_IP_ADDRESS]:8006/api2/json"
+  pm_user         = "root@pam"
+  pm_password     = "######"
+  pm_tls_insecure = true
+}
