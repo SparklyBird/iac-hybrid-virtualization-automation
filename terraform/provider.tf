66a8dddda9f1a6d4c73d037c8b75eca859f30328
SparklyBird <70488955+SparklyBird@users.noreply.github.com>
2025-06-08 02:21:08 +0300

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,15 +1,29 @@
+# This file defines all the providers required for the project.
+
 terraform {
   required_providers {
+    # Provider for Proxmox VE
     proxmox = {
       source  = "Telmate/proxmox"
       version = "2.9.14"
     }
+    # Provider for Docker
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
   }
 }
 
+# Configuration for the Proxmox provider
 provider "proxmox" {
-  pm_api_url      = "https://[PROXMOX_ZEROTIER_IP_ADDRESS]:8006/api2/json"
+  pm_api_url      = "https://[YOUR_PROXMOX_IP_ADDRESS]:8006/api2/json"
   pm_user         = "root@pam"
   pm_password     = "######"
   pm_tls_insecure = true
+}
+
+# Configuration for the Docker provider
+provider "docker" {
+  host = "tcp://[DOCKER_ZEROTIER_IP_ADDRESS]:2375"
 }
